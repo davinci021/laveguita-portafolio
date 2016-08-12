7929a166265b7c2fc3aa4bd9dd254c0248d724aa
davinci021 <david_zamo@hotmail.com>
2016-08-12 00:13:23 -0400

    Add files via upload

---
+++ BibliotecaADO/Conexion.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Oracle.DataAccess.Client;
+namespace BibliotecaADO
+{
+    public class Conexion
+    {
+        public OracleConnection m;
+        private static OracleConnection conexion1 = new OracleConnection();
+
+        public static OracleConnection Conexion2
+        {
+            get { return Conexion.conexion1; }
+            set { Conexion.conexion1 = value; }
+        }
+
+        public OracleConnection CConexion()
+        {
+            OracleConnection cn = new OracleConnection("Data Source=" + "DESKTOP-GVTK485" + "; User Id=" + "LAVEGUITA" + "; Password=" + "123456" + ";");
+            return cn;
+        }
+
+        public static void rutaConexion()
+        {
+            Conexion2.ConnectionString = "Data Source=" + "DESKTOP-GVTK485" + "; User Id=" + "LAVEGUITA" + "; Password=" + "123456" + ";";
+        }
+
+        public bool OpenConnection(string Servidor, string usuario, string pass)
+        {
+            try
+            {
+                m = new OracleConnection("Data Source=" + "DESKTOP-GVTK485" + "; User Id=" + "LAVEGUITA" + "; Password=" + "123456" + ";");
+                m.Open();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool closeConnection()
+        {
+            try
+            {
+                m.Close();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
